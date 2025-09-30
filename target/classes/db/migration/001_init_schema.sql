a871e5d76b5aa4cd601d1d09ce5a1f27f31f0d6d
herbal-egress <herbal.egress@gmail.com>
2025-09-30 14:23:41 +0500

    commit

---
+++ target/classes/db/migration/001_init_schema.sql
@@ -0,0 +1,15 @@
+-- добавленный код: Создание схемы bankrest.
+CREATE SCHEMA IF NOT EXISTS bankrest;
+
+-- добавленный код: Установка схемы по умолчанию.
+SET search_path TO bankrest;
+
+-- добавленный код: Таблица cards (пустая, будет доработана в этапе 2).
+CREATE TABLE IF NOT EXISTS cards (
+                                     id BIGSERIAL PRIMARY KEY
+);
+
+-- добавленный код: Таблица transactions (пустая, будет доработана).
+CREATE TABLE IF NOT EXISTS transactions (
+                                            id BIGSERIAL PRIMARY KEY
+);
