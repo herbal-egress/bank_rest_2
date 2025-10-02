f6ebfa73e5fbc87899b428ac08358f730e391f43
herbal-egress <herbal.egress@gmail.com>
2025-10-02 14:39:47 +0500

    Этап 5.1. Swagger

--- src/main/java/com/example/bankcards/dto/LoginRequestDTO.java
+++ src/main/java/com/example/bankcards/dto/LoginRequestDTO.java
@@ -8,7 +8,7 @@ public class LoginRequestDTO {
     @Size(min = 3, max = 50, message = "Имя пользователя должно быть от 3 до 50 символов")
     private String username;
     @NotBlank(message = "Пароль не может быть пустым")
-    @Size(min = 6, max = 100, message = "Пароль должен быть от 6 до 100 символов")
+    @Size(min = 3, max = 100, message = "Пароль должен быть от 3 до 100 символов")
     private String password;
 
     public String getUsername() {
