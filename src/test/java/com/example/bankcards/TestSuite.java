38345cb5a5417e6d494a52f239ce84c98d21cb9e
herbal-egress <herbal.egress@gmail.com>
2025-10-18 17:14:39 +0500

    Тест. AuthControllerTest

--- src/test/java/com/example/bankcards/TestSuite.java
+++ src/test/java/com/example/bankcards/TestSuite.java
@@ -4,14 +4,24 @@ import com.example.bankcards.controller.AdminControllerTest;
 import com.example.bankcards.controller.AuthControllerTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Тестовый набор для запуска всех тестов с единым контекстом
- * Добавлено: объединение тестов AuthControllerTest и AdminControllerTest
+ использован SpringBootTest вместо Suite для полной загрузки контекста
  */
+@SpringBootTest
+@ActiveProfiles("test")
 @Suite
 @SelectClasses({AuthControllerTest.class, AdminControllerTest.class})
-@ActiveProfiles("test")
 public class TestSuite {
+    private static final Logger logger = LoggerFactory.getLogger(TestSuite.class);
+
+    // Добавлено: отладочный вывод при запуске набора тестов
+    static {
+        logger.info("Запуск тестового набора TestSuite с SpringBootTest");
+    }
 }
