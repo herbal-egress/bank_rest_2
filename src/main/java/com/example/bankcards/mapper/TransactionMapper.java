02d4748763aba696330f180f12eb5fb2c7fe8018
herbal-egress <herbal.egress@gmail.com>
2025-10-26 17:44:21 +0500

    Тесты готовы.

--- src/main/java/com/example/bankcards/mapper/TransactionMapper.java
+++ src/main/java/com/example/bankcards/mapper/TransactionMapper.java
@@ -3,14 +3,15 @@ import com.example.bankcards.dto.TransactionDTO;
 import com.example.bankcards.entity.Transaction;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
-    @Mapping(source = "fromCardId", target = "fromCard.id") 
-    @Mapping(source = "toCardId", target = "toCard.id") 
-    @Mapping(target = "id", ignore = true) 
+    @Mapping(source = "fromCardId", target = "fromCard.id")
+    @Mapping(source = "toCardId", target = "toCard.id")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "timestamp", ignore = true) // изменил: игнорируем, так как задаётся в сервисе
+    @Mapping(target = "status", ignore = true) // изменил: игнорируем, так как задаётся в сервисе
     Transaction toEntity(TransactionDTO dto);
-    @Mapping(source = "fromCard.id", target = "fromCardId") 
-    @Mapping(source = "toCard.id", target = "toCardId") 
+    @Mapping(source = "fromCard.id", target = "fromCardId")
+    @Mapping(source = "toCard.id", target = "toCardId")
     TransactionDTO toDto(Transaction entity);
 }
